e9307feeda26f19f5f6c9eea8220bedcd2dd3d35
pablo javier jimenez <pablojavierjimenez@gmail.com>
2018-04-23 08:46:56 -0300

    ADD: target-colot container

---
+++ app/src/modules/target-color-container/target-color.component.js
@@ -0,0 +1,32 @@
+/**
+ * imports
+ */
+import { $, createDomElement, Component } from '../../helpers/helper';
+import './target-color.style.css';
+
+export class TargetColorComponent {
+  constructor(dom, domSelector = '#targetColorContainer') {
+    this.neme = 'targetColorComponent';
+    this.dom = dom;
+    this.domSelector = domSelector;
+    this.domSelectorId = `#${domSelector}`;
+    this.domSelectorClass = `.${domSelector}`;
+    // targetColor HTML TAG
+    this.targetColorContainer = createDomElement({
+      elementType: 'section',
+      attributeObj: {
+        class: `${this.domSelector}__container`
+      }
+    });
+
+    // adding a modal to body
+    Component('app-target-color', this.targetColorContainer);
+    this._setTargetColorContent('targetColor Works!!');
+  }
+
+  _setTargetColorContent(newContent = '') {
+    let template = `
+      <span class="app_targetColor__title">${newContent}</span>`;
+    return $('.app_targetColor__container').html(template);
+  }
+}
